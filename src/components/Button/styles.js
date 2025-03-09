06bdc708c386e7262f3f7f1c0a9ded6b672de08e
matheus dos santos silva <mathequias23@gmail.com>
2025-03-09 18:12:48 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+export const ButtonContainer = styled.button`
+    padding: 26px;
+    flex: 1;
+    border: 1px solid #cdcdcd;
+    background-color:#1962ff;
+    color: #ffffff;
+    font-size: 30px;
+    font-weight: 400;
+
+    &:hover{
+        opacity: 0.6;
+    }
+`
---
+++ src/components/input/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+
+    font-size: 32px;
+    
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    position: relative;
+
+    input {
+        width: 100%;
+        height: 75px;
+        border: 0;
+        background-color: #AAFFAA;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 32px;
+        font-family: 'Roboto';
+
+    }
+`
